8b715b7b5830c016b46e44d2a15de77dc205a863
jok999 <42048536+jok999@users.noreply.github.com>
2018-10-18 21:10:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -137,7 +137,6 @@ message.channel.sendFile(canvas.toBuffer());
  
 
 const moment = require("moment");  
-const fs = require("fs");      
 const dateFormat = require('dateformat');
 const Canvas = require("canvas");
 let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
