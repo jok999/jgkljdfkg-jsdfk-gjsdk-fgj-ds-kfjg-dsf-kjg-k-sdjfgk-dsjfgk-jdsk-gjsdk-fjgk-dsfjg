15a5b1610377f2e1782927d877f561d22239de4d
jok999 <42048536+jok999@users.noreply.github.com>
2018-10-19 18:14:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,133 +1,119 @@
-const fs = require("fs"); //npm i fs
-const Canvas = require("canvas");//for linux = npm i canvas | for windows = npm i canvas-prebuilt
-const jimp = require("jimp");// npm i jimp
-const prefix = "="; // prefix
-const Discord = require('discord.js');// npm i discord.js
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const id = JSON.parse(fs.readFileSync("./id/mozo.json", "utf8"));
-client.on("message", message => {
-  if (message.author.bot) return;
-fs.writeFile('./id/mozo.json', JSON.stringify(id), (err) => {
-if (err) console.error(err);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+    if (message.content.startsWith("!readyserver")) {
+     if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
+     message.guild.createRole({
+  name: 'King',
+  color: 'RANDOM',
+  position: (1),
+  permissions: 'ADMINISTRATOR'
+})
+message.guild.createRole({
+    name: 'Prince',
+    color: 'RANDOM',
+    position: (2),
+    permissions: ['CREATE_INSTANT_INVITE', 'KICK_MEMBERS', 'BAN_MEMBERS', 'MANAGE_CHANNELS', 'ADD_REACTIONS', 'VIEW_AUDIT_LOG', 'VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES',
+        'SEND_TTS_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS',
+         'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'MANAGE_NICKNAMES'],
+})
+message.guild.createRole({
+    name: 'Commander',
+    color: 'RANDOM',
+     position: (3),
+    permissions: ['CREATE_INSTANT_INVITE', 'KICK_MEMBERS', 'ADD_REACTIONS', 'VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS',
+    'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'MANAGE_NICKNAMES']
+})
+message.guild.createRole({
+    name: 'Admin',
+    color: 'RANDOM',
+    postion: (4),
+    permissions: ['VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS',
+    'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME']
+})
+message.guild.createRole({
+    name: 'Vip',
+    color: 'RANDOM',
+    postion: (5),
+    permissions: ['VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS',
+    'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME']
+})
+message.guild.createRole({
+    name: 'Active',
+    color: '#030303',
+    postion: (6),
+    permissions: ['VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'CONNECT', 'SPEAK',
+    'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'ADD_REACTIONS']
+})
+message.channel.send('**Roles Was Succsesfluy Created**')
+.catch(console.error);
+    }
 });
-      client.on('message', message => {
-          if(!id[message.author.id]) id[message.author.id] ={
-              textrank: 1,
-              points: 1
-          };
-          if(message.author.bot) return;
-          id[message.author.id].points = Math.floor(id[message.author.id].points+4);
-          if(id[message.author.id].points > 10) {
-              id[message.author.id].points = 10;
-              id[message.author.id].level = Math.floor(id[message.author.id].level+4);
-          }
-          fs.writeFile('./id/mozo.json', JSON.stringify(id), (err) => {
-if (err) console.error(err);
-});
-   
-    client.on("message", message => {
-  if (message.author.bot) return;
-    if(!message.channel.guild) return;
-if (message.content.startsWith(prefix + "id")) {
-                               let user = message.mentions.users.first();
-         var human = message.mentions.users.first();
-            var author;
-            if(human) {
-                author = human;
-            } else {
-                author = message.author;
-            }
-          var mentionned = message.mentions.members.first();
-             var ah;
-            if(mentionned) {
-                ah = mentionned;
-            } else {
-                ah = message.member;
-            }
-            var ment = message.mentions.users.first();
-            var getvalueof;
-            if(ment) {
-              getvalueof = ment;
-            } else {
-              getvalueof = message.author;
-            }
-   var mentionned = message.mentions.users.first();
- 
-    var client;
-      if(mentionned){
-          var client = mentionned;
-      } else {
-          var client = message.author;
- 
-      }
-if (!id[getvalueof.id]) id[getvalueof.id] = {textrank: 0,points: 1};
-            let Image = Canvas.Image,
-            canvas = new Canvas(400, 200),
-            ctx = canvas.getContext('2d');
-            fs.readFile("./id/rank.png", function (err, Background) {
-            if (err) return console.log(err);
-            let id = Canvas.Image;
-            let ground = new Image;
-            ground.src = Background;
-            ctx.drawImage(ground, 0, 0, 400, 200);
- 
-});
+client.on('message', message => {
+    if (message.content.startsWith("!readyserver")) {
+    if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`${message.author.username} You Dont Have ``MANAGE_CHANNELS`` **Premission**`);
+     message.guild.createChannel('rules', 'text')
+     message.guild.createChannel('welcome', 'text')
+     message.guild.createChannel('chat', 'text')
+     message.guild.createChannel('youtubers', 'text')
+     message.guild.createChannel('giveaway', 'text')
+     message.guild.createChannel('shop', 'text')
+     message.guild.createChannel('bot', 'text')
+     
  
  
+     
  
-                let url = getvalueof.displayAvatarURL.endsWith(".webp") ? getvalueof.displayAvatarURL.slice(5, -20) + ".png" : getvalueof.displayAvatarURL;
-                jimp.read(url, (err, ava) => {
-                    if (err) return console.log(err);
-                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                        if (err) return console.log(err);
- 
-                        // N A M E  |  S H A D O W
-                        ctx.font = 'bold 18px Arial';
-                        ctx.fontSize = '18px';
-                        ctx.fillStyle = "#000000";
-                        ctx.textAlign = "center";
-                        ctx.fillText(`${getvalueof.username}`, 253, 79);
- 
-                        // N A M E
-                        ctx.font = 'bold 18px Arial';
-                        ctx.fontSize = '18px';
-                        ctx.fillStyle = "#f1f1f1";
-                        ctx.textAlign = "center";
-                        ctx.fillText(`${getvalueof.username}`, 253, 77);
- 
- 
-                        // T E X T  R A N K
-                        ctx.font = "bold 12px Arial";
-                        ctx.fontSize = '12px';
-                        ctx.fillStyle = "#f1f1f1";
-                        ctx.textAlign = "center";
-                        ctx.fillText(`${id[getvalueof.id].textrank}`, 252, 124);
- 
-                        // P O I N T S
-                        ctx.font = "bold 12px Arial";
-                        ctx.fontSize = '12px';
-                        ctx.fillStyle = "#f1f1f1";
-                        ctx.textAlign = "center";
-                        ctx.fillText(`${id[getvalueof.id].points}`, 253, 171);
- 
- 
-                        let Avatar = Canvas.Image;
-                        let ava = new Avatar;
- 
-ava.src = buf;
-                        ctx.beginPath();
-                        ctx.arc(75, 100, 780, 0, Math.PI*2, true);
-                        ctx.closePath();
-                        ctx.clip();
-                        ctx.drawImage(ava, 26, 69, 93, 93);
-                       
-message.channel.sendFile(canvas.toBuffer());
- 
-});
-});
+message.channel.sendMessage('**Channels Was Succsesfluy Created**')
 }
 });
+ 
+client.on('message', message => {
+    if (message.content.startsWith("!readyserver")) {
+    if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
+message.guild.createChannel('╔╣MUSIC╠╗', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣Public╠╗', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣MineCraft╠╗', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣Fortnite╠╗', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣Call Of Duty╠╗', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣League Of Legends╠╗', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣Battle Field╠╗', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣Cs : Go╠╗', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+.catch(console.error);
+    }
 });
 
 
